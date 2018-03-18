658fb7cacf2b0beed51dba2e921ce7c755630b94
Max Humber <max@max-mbp.local>
2018-03-18 11:51:26 -0400

    more packaging boilerplate

--- setup.py
+++ setup.py
@@ -1,13 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
 
 setup(
     name='mummify',
-    version='0.1',
+    version='0.0.1',
     description='Automatic ML Logging',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Version Control',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
     url='https://github.com/maxhumber/mummify',
     author='Max Humber',
     author_email='max.humber@gmail.com',
     license='MIT',
     packages=['mummify'],
+    install_requires=[
+        'markdown',
+    ],
+    include_package_data=True,
     zip_safe=False
 )
