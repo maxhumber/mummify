ad837932707d5c2ee1a8cda727c2e132d39f5909
Max Humber <max.humber@gmail.com>
2018-03-20 23:19:36 -0400

    1/3 test

--- test/maker.py
+++ test/maker.py
@@ -1,33 +1,40 @@
 import subprocess
+import re
+
 file = 'model.py'
-
-# instantiate
-with open(file, 'w+') as f:
-    f.seek(0, 0)
-
-# first write
 contents = '''import mummify
 train_accuracy = 0.80
 test_accuracy = 0.80
 mummify.log(f'Train: {train_accuracy:.3f}, Test: {test_accuracy:.3f}')
 '''
 
-with open(file, "w") as f:
+# instantiate 'model'
+with open(file, 'w+') as f:
     f.write(contents)
 
-subprocess.call(['python model.py'], shell=True)
+# first mummify log
+subprocess.call(['python model.py &>/dev/null'], shell=True)
+subprocess.call(["echo 'maker.py' >> .gitignore"], shell=True)
+
+# change
+def fake_change(file, previous, new):
+    with open(file, "r+") as f:
+        contents = f.read()
+        contents = contents.replace(previous, new)
+        f.seek(0)
+        f.write(contents)
+        f.truncate()
+
+fake_change(file, '0.80', '0.750')
+
+subprocess.call(['python model.py &>/dev/null'], shell=True)
+
+fake_change(file, '0.750', '0.767')
+
+history = subprocess.check_output(['mummify history'], shell=True).decode('utf-8').strip()
+mummify_id = re.search('(?<=\n\*\s\s)(.*)(?=\n\*\s\smummify-start)', history).group(0)
 
 
-# first change
-with open('model.py', )
-
-
-with open(f'manuscript/{notebook}.txt', encoding='UTF-8') as f:
-    chapter = f.read()
-
-# correct image locations
-chapter = chapter.replace(f'({notebook}_files/', '(images/')
-chapter = chapter.replace(f'(../manuscript/', '(')
-chapter = chapter.replace(f'![png](', '![](')
-
-with open()
+def tear_down():
+    subprocess.call(['rm .git .gitignore model.py mummify.log'], shell=True)
+    subprocess.call(['rm -rf .mummify'], shell=True)
