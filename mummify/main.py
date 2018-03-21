80ef169c6a33c85e83a6cb5558abea2f3d398b14
Max Humber <max.humber@gmail.com>
2018-03-20 21:28:43 -0400

    fix init/branch

--- mummify/main.py
+++ mummify/main.py
@@ -1,17 +1,18 @@
-import os
 import logging
 import uuid
 import re
-from subprocess import call, check_output, CalledProcessError
+import subprocess
+from ast import literal_eval
 
 LOGFILE = 'mummify.log'
 
-def shell(command, silent=True, output=False):
+def shell(command, capture_output=False, silent=True):
+    '''Execute bash commands'''
     if type(command) is str:
         command = [command]
-    if output:
+    if capture_output:
         o = [
-            check_output([c], shell=True)
+            subprocess.check_output([c], shell=True)
             .decode('utf-8')
             .strip()
             for c in command]
@@ -19,54 +20,69 @@ def shell(command, silent=True, output=False):
             return o[0]
         else:
             return o
-    else:
-        [call([c], shell=True) for c in command]
-        return None
-
-def _find(id):
-    log_item = shell(f'git log --all --grep={id}', output=True)
-    commit = re.findall('(?<=commit\s)(.*?)(?=\n)',log_item)[0]
-    return commit
+    if silent:
+        command = [c + ' &>/dev/null' for c in command]
+    [subprocess.call([c], shell=True) for c in command]
+    return None
 
 def view():
-    graph = shell('git log --graph --decorate --oneline', output=True)
+    '''CLI - view modified git graph'''
+    graph = shell('git log --graph --decorate --oneline', capture_output=True)
     graph = re.sub('\s([a-zA-Z0-9_-]){7}\s', '  ', graph)
     graph = re.sub(r'\(HEAD -> master\)', 'CURRENT', graph)
     return '\n' + graph + '\n'
 
-#TODO: supress git messages
-#TODO: make this cleaner
+def find(id):
+    '''Find git commit id'''
+    log_item = shell(f'git log --all --grep={id}', capture_output=True)
+    commit = re.findall('(?<=commit\s)(.*?)(?=\n)',log_item)[0]
+    return commit
+
 def switch(id):
-    commit = _find(id)
-    shell('git checkout -b log --quiet')
-    shell('git checkout -b switch --quiet')
-    shell(f'git reset --hard {commit} --quiet')
-    shell('git merge -s ours --no-commit master --quiet')
-    shell(f'git checkout log {LOGFILE} --quiet')
-    #TODO: remove mummify- ?
-    shell(f'git commit -m "switch-{id}" --quiet')
-    shell('git checkout master --quiet')
-    shell('git merge switch --quiet')
-    shell('git branch -D log --quiet')
-    shell('git branch -D switch --quiet')
+    '''CLI - switch to mummify commit'''
+    commit = find(id)
+    shell([
+        'git checkout -b logger',
+        'git checkout -b switch',
+        f'git reset --hard {commit}',
+        'git merge -s ours --no-commit master',
+        f'git checkout log {LOGFILE}',
+        f'git commit -m "switch-{id}"',
+        'git checkout master',
+        'git merge switch',
+        'git branch -D logger',
+        'git branch -D switch'
+    ])
 
-#TODO: fix the git fatal message
-def _create_branch(BRANCH):
-    try:
-        shell(f'git checkout -b {BRANCH} --quiet', output=True)
-    except CalledProcessError:
-        shell('git init --quiet')
-        shell('git add .')
-        shell('git commit -m "mummify-start" --quiet')
+def create_branch(BRANCH):
+    '''Create new mummify branch'''
+    git = (
+        subprocess.Popen(
+            'git status >/dev/null 2>&1 && echo True',
+            shell=True,
+            stdout=subprocess.PIPE
+        ).communicate()[0].decode('utf-8').strip())
+    if git:
+        shell(f'git checkout -b {BRANCH}')
+    else:
+        shell([
+            'git init',
+            'git add .',
+            'git commit -m "mummify-start"'
+        ])
 
-def _commit(BRANCH):
-    shell('git add .')
-    shell(f'git commit -m {BRANCH} --quiet')
-    shell('git checkout master --quiet')
-    shell(f'git merge {BRANCH} --quiet')
-    shell(f'git branch -d {BRANCH} --quiet')
+def commit(BRANCH):
+    '''Commit run to mummify'''
+    shell([
+        'git add .',
+        f'git commit -m {BRANCH} --quiet',
+        'git checkout master --quiet',
+        f'git merge {BRANCH} --quiet',
+        f'git branch -d {BRANCH} --quiet'
+    ])
 
 def log(message):
+    '''Main interface'''
     BRANCH = 'mummify-' + str(uuid.uuid4().hex)[:8]
     logger = logging.getLogger(BRANCH)
     logging.basicConfig(
@@ -75,6 +91,6 @@ def log(message):
         style='{',
         format='{name}|{message}'
     )
-    _create_branch(BRANCH)
+    create_branch(BRANCH)
     logger.info(message)
-    _commit(BRANCH)
+    commit(BRANCH)
