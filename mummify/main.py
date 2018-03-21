2de7f8568affc65900ddb04431f9a29d46f5c9cf
Max Humber <max.humber@gmail.com>
2018-03-20 20:45:44 -0400

    have shell execute accept multiple commands

--- mummify/main.py
+++ mummify/main.py
@@ -6,20 +6,30 @@ from subprocess import call, check_output, CalledProcessError
 
 LOGFILE = 'mummify.log'
 
-#TODO: accept a list of commands
-def _execute(command, output=False):
+def shell(command, silent=True, output=False):
+    if type(command) is str:
+        command = [command]
     if output:
-        return check_output([command], shell=True).decode('utf-8').strip()
+        o = [
+            check_output([c], shell=True)
+            .decode('utf-8')
+            .strip()
+            for c in command]
+        if len(o) == 1:
+            return o[0]
+        else:
+            return o
     else:
-        call([command], shell=True)
+        [call([c], shell=True) for c in command]
+        return None
 
 def _find(id):
-    log_item = _execute(f'git log --all --grep={id}', output=True)
+    log_item = shell(f'git log --all --grep={id}', output=True)
     commit = re.findall('(?<=commit\s)(.*?)(?=\n)',log_item)[0]
     return commit
 
 def view():
-    graph = _execute('git log --graph --decorate --oneline', output=True)
+    graph = shell('git log --graph --decorate --oneline', output=True)
     graph = re.sub('\s([a-zA-Z0-9_-]){7}\s', '  ', graph)
     graph = re.sub(r'\(HEAD -> master\)', 'CURRENT', graph)
     return '\n' + graph + '\n'
@@ -28,33 +38,33 @@ def view():
 #TODO: make this cleaner
 def switch(id):
     commit = _find(id)
-    _execute('git checkout -b log --quiet')
-    _execute('git checkout -b switch --quiet')
-    _execute(f'git reset --hard {commit} --quiet')
-    _execute('git merge -s ours --no-commit master --quiet')
-    _execute(f'git checkout log {LOGFILE} --quiet')
+    shell('git checkout -b log --quiet')
+    shell('git checkout -b switch --quiet')
+    shell(f'git reset --hard {commit} --quiet')
+    shell('git merge -s ours --no-commit master --quiet')
+    shell(f'git checkout log {LOGFILE} --quiet')
     #TODO: remove mummify- ?
-    _execute(f'git commit -m "switch-{id}" --quiet')
-    _execute('git checkout master --quiet')
-    _execute('git merge switch --quiet')
-    _execute('git branch -D log --quiet')
-    _execute('git branch -D switch --quiet')
+    shell(f'git commit -m "switch-{id}" --quiet')
+    shell('git checkout master --quiet')
+    shell('git merge switch --quiet')
+    shell('git branch -D log --quiet')
+    shell('git branch -D switch --quiet')
 
 #TODO: fix the git fatal message
 def _create_branch(BRANCH):
     try:
-        _execute(f'git checkout -b {BRANCH} --quiet', output=True)
+        shell(f'git checkout -b {BRANCH} --quiet', output=True)
     except CalledProcessError:
-        _execute('git init --quiet')
-        _execute('git add .')
-        _execute('git commit -m "mummify-start" --quiet')
+        shell('git init --quiet')
+        shell('git add .')
+        shell('git commit -m "mummify-start" --quiet')
 
 def _commit(BRANCH):
-    _execute(f'git add .')
-    _execute(f'git commit -m {BRANCH} --quiet')
-    _execute(f'git checkout master --quiet')
-    _execute(f'git merge {BRANCH} --quiet')
-    _execute(f'git branch -d {BRANCH} --quiet')
+    shell('git add .')
+    shell(f'git commit -m {BRANCH} --quiet')
+    shell('git checkout master --quiet')
+    shell(f'git merge {BRANCH} --quiet')
+    shell(f'git branch -d {BRANCH} --quiet')
 
 def log(message):
     BRANCH = 'mummify-' + str(uuid.uuid4().hex)[:8]
