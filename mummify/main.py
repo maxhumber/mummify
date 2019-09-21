b16bbdddf72729cb860657c0b6f39635177dbda9
Max Humber <max.humber@gmail.com>
2019-09-21 16:28:55 -0400

    silence

--- mummify/main.py
+++ mummify/main.py
@@ -6,17 +6,15 @@ import uuid
 
 LOGFILE = 'mummify.log'
 
-# TODO: fix silent
-def run(command, output=False, silent=True):
+def run(command, output=False, silent=False):
     '''Run shell commands'''
-    # if silent and not output:
-    #     command += ' &>/dev/null'
+    if silent:
+        command += ' &>/dev/null'
     if output:
         s = subprocess.run(command, capture_output=True, shell=True)
         output = s.stdout.decode('utf-8').strip()
         return output
-    else:
-        subprocess.run(command, shell=True)
+    subprocess.run(command, shell=True)
 
 def colour(string):
     '''Paint it purple!'''
@@ -24,12 +22,12 @@ def colour(string):
 
 def init_mummify():
     '''Initialize mummify'''
-    run('git init --separate-git-dir .mummify')
+    run('git init --separate-git-dir .mummify', silent=True)
     run("echo '.mummify' >> .gitignore")
     run("echo '__pycache__' >> .gitignore") # delete
     run('git --git-dir=.mummify add .gitignore')
-    run('git --git-dir=.mummify commit -m "mummify-root"')
-    colour('mummify initialized')
+    run('git --git-dir=.mummify commit -m "mummify-root"', silent=True)
+    colour('Initializing mummify')
 
 def history():
     '''View modified git graph (CLI)'''
@@ -38,7 +36,7 @@ def history():
         output=True)
     graph = re.sub(r'\s([a-zA-Z0-9_-]){7}\s', '  ', graph)
     graph = re.sub(r'\(HEAD -> master\)', 'HEAD', graph)
-    print(f'\n{graph}\n')
+    return f'\n{graph}\n'
 
 def check_status():
     '''Check mummify git status'''
@@ -47,7 +45,7 @@ def check_status():
 
 def create_branch(BRANCH):
     '''Create new mummify branch'''
-    run(f'git --git-dir=.mummify checkout -b {BRANCH}')
+    run(f'git --git-dir=.mummify checkout -b {BRANCH}', silent=True)
 
 def commit(BRANCH):
     '''Commit run to .mummify'''
@@ -68,16 +66,17 @@ def find(id):
 def switch(id):
     '''Switch to a specific mummify commit (CLI)'''
     commit = find(id)
-    run('git --git-dir=.mummify checkout -b logger')
-    run('git --git-dir=.mummify checkout -b switcher')
-    run(f'git --git-dir=.mummify reset --hard {commit}')
-    run('git --git-dir=.mummify merge -s ours --no-commit master')
-    run(f'git --git-dir=.mummify checkout logger {LOGFILE}')
-    run(f'git --git-dir=.mummify commit -m "switch-{id}"')
-    run('git --git-dir=.mummify checkout master')
-    run('git --git-dir=.mummify merge switcher')
-    run('git --git-dir=.mummify branch -D logger')
-    run('git --git-dir=.mummify branch -D switcher')
+    run('git --git-dir=.mummify checkout -b logger', silent=True)
+    run('git --git-dir=.mummify checkout -b switcher', silent=True)
+    run(f'git --git-dir=.mummify reset --hard {commit}', silent=True)
+    run('git --git-dir=.mummify merge -s ours --no-commit master', silent=True)
+    run(f'git --git-dir=.mummify checkout logger {LOGFILE}', silent=True)
+    run(f'git --git-dir=.mummify commit -m "switch-{id}"', silent=True)
+    run('git --git-dir=.mummify checkout master', silent=True)
+    run('git --git-dir=.mummify merge switcher', silent=True)
+    run('git --git-dir=.mummify branch -D logger', silent=True)
+    run('git --git-dir=.mummify branch -D switcher', silent=True)
+    return colour(f'Sucessfully switched to {id}')
 
 def log(message):
     '''Log a message to mummify.log and save a snapshot'''
