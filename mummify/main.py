2650dec46c6a94c4bb42d67b0badf1e28fb88236
Max Humber <max.humber@gmail.com>
2019-09-21 13:03:59 -0400

    switch everything to subprocess.run

--- mummify/main.py
+++ mummify/main.py
@@ -6,93 +6,78 @@ import uuid
 
 LOGFILE = 'mummify.log'
 
-def shell(command, capture_output=False, silent=True):
-    '''Execute bash commands'''
-    if type(command) is str:
-        command = [command]
-    if capture_output:
-        o = [subprocess.check_output([c], shell=True)
-            .decode('utf-8')
-            .strip()
-            for c in command]
-        if len(o) == 1:
-            return o[0]
-        else:
-            return o
-    if silent:
-        command = [c + ' &>/dev/null' for c in command]
-    [subprocess.call([c], shell=True) for c in command]
+def run(command, output=False, silent=True):
+    '''Run shell commands'''
+    # if silent and not output:
+    #     command += ' &>/dev/null'
+    if output:
+        s = subprocess.run(command, capture_output=True, shell=True)
+        output = s.stdout.decode('utf-8').strip()
+        return output
+    else:
+        subprocess.run(command, shell=True)
 
 def colour(string):
     '''Paint it purple!'''
-    print('\033[35m' + string + '\033[0m')
+    print(f"\033[35m{string}\033[0m")
+
+def init_mummify():
+    '''Initialize mummify'''
+    run('git init --separate-git-dir .mummify')
+    run("echo '.mummify' >> .gitignore")
+    run("echo 'main.py' >> .gitignore") # delete after testing
+    run("echo '__pycache__' >> .gitignore") # delete after testing
+    run('git --git-dir=.mummify add .gitignore')
+    run('git --git-dir=.mummify commit -m "mummify-initialized"')
+    colour('mummify initialized')
 
 def history():
     '''View modified git graph (CLI)'''
-    graph = shell(
+    graph = run(
         'git --git-dir=.mummify log --graph --decorate --oneline',
-        capture_output=True)
+        output=True)
     graph = re.sub(r'\s([a-zA-Z0-9_-]){7}\s', '  ', graph)
     graph = re.sub(r'\(HEAD -> master\)', 'HEAD', graph)
-    return '\n' + graph + '\n'
+    print(f'\n{graph}\n')
+
+def check_status():
+    '''Check mummify git status'''
+    git_status = run("git --git-dir=.mummify status", output=True)
+    return git_status
+
+def create_branch(BRANCH):
+    '''Create new mummify branch'''
+    run(f'git --git-dir=.mummify checkout -b {BRANCH}')
+
+def commit(BRANCH):
+    '''Commit run to .mummify'''
+    run('git --git-dir=.mummify add .')
+    run(f'git --git-dir=.mummify commit -m {BRANCH} --quiet')
+    run('git --git-dir=.mummify checkout master --quiet')
+    run(f'git --git-dir=.mummify merge {BRANCH} --quiet')
+    run(f'git --git-dir=.mummify branch -d {BRANCH} --quiet')
 
 def find(id):
     '''Find git commit based on mummify id'''
-    log_item = shell(
+    log_item = run(
         f'git --git-dir=.mummify log --all --grep={id}',
-        capture_output=True)
-    commit = re.findall(r'(?<=commit\s)(.*?)(?=\n)',log_item)[0]
+        output=True)
+    commit = re.findall(r'(?<=commit\s)(.*?)(?=\n)', log_item)[0]
     return commit
 
 def switch(id):
     '''Switch to a specific mummify commit (CLI)'''
     commit = find(id)
-    shell([
-        'git --git-dir=.mummify checkout -b logger',
-        'git --git-dir=.mummify checkout -b switcher',
-        f'git --git-dir=.mummify reset --hard {commit}',
-        'git --git-dir=.mummify merge -s ours --no-commit master',
-        f'git --git-dir=.mummify checkout logger {LOGFILE}',
-        f'git --git-dir=.mummify commit -m "switch-{id}"',
-        'git --git-dir=.mummify checkout master',
-        'git --git-dir=.mummify merge switcher',
-        'git --git-dir=.mummify branch -D logger',
-        'git --git-dir=.mummify branch -D switcher'
-    ])
-
-def init_mummify():
-    '''Initialize mummify if not exists'''
-    shell('git init --separate-git-dir .mummify')
-    shell("echo '.mummify' >> .gitignore", silent=False)
-    shell([
-        'git --git-dir=.mummify add .',
-        'git --git-dir=.mummify commit -m "mummify-start"'
-    ])
-    colour('mummify successfully initialized!')
-    return None
-
-def create_branch(BRANCH):
-    '''Create new mummify branch'''
-    shell(f'git --git-dir=.mummify checkout -b {BRANCH}')
-
-def commit(BRANCH):
-    '''Commit run to .mummify'''
-    shell([
-        'git --git-dir=.mummify add .',
-        f'git --git-dir=.mummify commit -m {BRANCH} --quiet',
-        'git --git-dir=.mummify checkout master --quiet',
-        f'git --git-dir=.mummify merge {BRANCH} --quiet',
-        f'git --git-dir=.mummify branch -d {BRANCH} --quiet'
-    ])
-
-def check_status():
-    '''Check mummify git status'''
-    git_status = subprocess.Popen(
-        "git --git-dir=.mummify status | grep 'nothing to commit'",
-        shell=True,
-        stdout=subprocess.PIPE
-    ).communicate()[0].decode('utf-8').strip()
-    return git_status
+    run('git --git-dir=.mummify checkout -b logger')
+    run('git --git-dir=.mummify checkout -b switcher')
+    run(f'git --git-dir=.mummify reset --hard {commit}')
+    run('git --git-dir=.mummify merge -s ours --no-commit master')
+    run(f'git --git-dir=.mummify checkout logger {LOGFILE}')
+    run(f'git --git-dir=.mummify commit -m "switch-{id}"')
+    run('git --git-dir=.mummify checkout master')
+    run('git --git-dir=.mummify merge switcher')
+    run('git --git-dir=.mummify branch -D logger')
+    run('git --git-dir=.mummify branch -D switcher')
 
 def log(message):
     '''Main application interface'''
@@ -102,17 +87,17 @@ def log(message):
         style='{',
         format='[{name}] {message}'
     )
-    BRANCH = 'mummify-' + str(uuid.uuid4().hex)[:8]
+    BRANCH = f'mummify-{str(uuid.uuid4().hex)[:8]}'
     logger = logging.getLogger(BRANCH)
     if not Path('.mummify').is_dir():
         init_mummify()
-        logger.info(message)
-        commit(BRANCH)
-        return
-    if check_status() == 'nothing to commit, working tree clean':
+    if 'nothing to commit' in check_status():
         colour('Nothing changed, nothing logged')
-        return
+        return None
     create_branch(BRANCH)
     logger.info(message)
     colour(message)
     commit(BRANCH)
+
+def cleanup():
+    run('rm -rf .gitignore .git .mummify mummify.log')
