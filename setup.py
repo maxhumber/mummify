d492924ba75ae3a55df4c36d0b59e6a28e6ca9c3
Max Humber <max.humber@gmail.com>
2019-09-30 11:16:22 -0500

    Deployed  with MkDocs version: 1.0.4

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='mummify',
+    version='1.0.1',
+    description='git + logging for ML',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Version Control',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    url='https://github.com/maxhumber/mummify',
+    author='Max Humber',
+    author_email='max.humber@gmail.com',
+    license='MIT',
+    packages=['mummify'],
+    entry_points = {
+        'console_scripts': ['mummify=mummify.cli:cli']
+    },
+    zip_safe=False,
+    python_requires='>=3.7',
+    setup_requires=['setuptools>=38.6.0']
+)
